d8f96034cc3d481c5165f3239c6a5009abe20faa
n.iskakov <iskakovnadim08@gmail.com>
2023-01-29 20:13:35 +0100

    recreated slice and main component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,30 +1,71 @@
-import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import { useDispatch, useSelector } from 'react-redux'
-import { setQuizData } from '../features/reducers/quiz';
+import {fetchData} from "../features/reducers/quiz";
+
+
+
 
 export const Quiz = ()=>{
     const dispatch = useDispatch();
+    const data = useSelector(state=> state.quiz.data)
+    const isLoading = useSelector(state=>state.quiz.isLoading)
+    const error = useSelector(state=> state.quiz.error)
+
+    const [quiz, setQuiz] = useState([])
+    const [points, setPoints] = useState(0)
 
     useEffect(() => {
-        axios('https://opentdb.com/api.php?amount=10')
-            .then((response) => response.data.results)
-            .then((result) => dispatch(setQuizData(result)));
-    }, []);
+        dispatch(fetchData())
+    }, [dispatch]);
 
+    const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
 
-    const quizData = useSelector((state) => state.quiz);
+    useEffect(()=>{
+        setQuiz(data.map(item=>(
+            {
+                question: item.question,
+                options: shuffle([...item.incorrect_answers, item.correct_answer]),
+                answer: item.correct_answer
+            }
+        )))
+    }, [data])
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    console.log(quiz)
+    console.log(quiz[0].answer)
+    const pickAnswer = (e)=>{
+        const pickAnswer= e.target.outerText
+        console.log(pickAnswer)
+        console.log(quiz[0].answer)
+        if(quiz[0].answer === pickAnswer){
+            console.log('true')
+            setPoints(points +1)
+        }else{
+            console.log('false')
+        }
+        console.log(points)
+    }
 
     return (
         <div>
-            <ul>
-                {quizData.map((question) => (
-                    <li key={question.question}>
-                        <div>{question.category}</div>
-                        <div>{question.correct_answer}</div>
-                    </li>
+            {quiz.map(item=>(
+                <>
+                    <h2>{item.question}</h2>
+                    <div>
+                        {item.options.map(option =>(
+                            <button onClick={pickAnswer}>{option}</button>
+                        ))}
+                    </div>
+                </>
+
                 ))}
-            </ul>
         </div>
     );
 };
--- src/features/reducers/quiz.jsx
+++ src/features/reducers/quiz.jsx
@@ -1,15 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from "axios";
+
+
+
+
+// Define the fetchData thunk using the createAsyncThunk function
+export const fetchData = createAsyncThunk('data/fetchData', async(payload, thunkAPI) =>{
+    const response = await axios.get('https://opentdb.com/api.php?amount=10')
+    // Return the data from the API response
+    return response.data.results;
+})
+
 
 export const quiz = createSlice({
     name: 'quiz',
-    initialState: [],
-    reducers: {
-        setQuizData: (state, action) => {
-            state.push(...action.payload);
-        },
+    initialState: {
+        data: [],
+        isLoading: false,
+        error: null
     },
+    reducers: {
+        fetchDataStart: (state)=>{
+            state.isLoading = true;
+        },
+        fetchDataSuccess:(state, action)=>{
+            // Set the data to the payload from the action
+            state.data = action.payload;
+            state.isLoading = false;
+            state.error = null;
+        },
+        fetchDataFailure: (state, action) =>{
+            state.error = action.payload;
+            state.isLoading = false;
+        }
+    },
+    // Extra reducers to handle the status of the fetchData thunk
+    extraReducers:{
+        [fetchData.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchData.fulfilled]: (state, action) => {
+            state.data = action.payload;
+            state.isLoading = false;
+            state.error = null;
+        },
+        [fetchData.rejected]: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+    }
 });
-
-export const { setQuizData } = quiz.actions;
+// export the actions and reducer
+export const { fetchDataStart, fetchDataSuccess, fetchDataFailure } = quiz.actions;
 
 export default quiz.reducer;
